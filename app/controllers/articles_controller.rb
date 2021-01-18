c313b1fc81447e201d55d604325777546de9cebc
Shaher-11 <shahershamroukh@gmail.com>
2021-01-18 12:31:42 +0200

    added the table

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+
+
+
+    def show
+        @article = Article.find(params[:id])
+
+        end 
+     
+        def index
+            @articles = Article.all
+        end 
+
+end 
