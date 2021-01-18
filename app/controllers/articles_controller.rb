9fcbf257c3a754b2a72be8b8fc53152291291e65
Shaher-11 <shahershamroukh@gmail.com>
2021-01-18 18:48:32 +0200

    added some more methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,11 +12,43 @@ class ArticlesController < ApplicationController
         end 
 
         def new
-
+            @article = Article.new
         end
-
+        def edit
+            @article = Article.find(params[:id])
+        end
         def create
-            @article = Article.new()
+            @article = Article.new(params.require(:article).permit(:title, :description))
+           if @article.save
+             flash[:notice] = "Article was created succefully."
+            redirect_to @article
+           else 
+            render 'new'
         end
+    end 
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+          flash[:notice] = "Article was updated successfully."
+          redirect_to @article
+        else
+          render 'edit'
+        end
+      end
+
+      def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+      end
 
 end 
+
+
+
+
+
+
+
+
