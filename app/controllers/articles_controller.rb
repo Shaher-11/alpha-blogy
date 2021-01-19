8820a100a0a47a48fbf52abc0329c25b2646d6fb
Shaher-11 <shahershamroukh@gmail.com>
2021-01-19 19:18:40 +0200

    cleanup layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,9 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def show
+      
         end 
-     
+       
         def index
             @articles = Article.all
         end 
