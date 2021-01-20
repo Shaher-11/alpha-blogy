bdf4cd9531061db21763c9c0d3839fbbab789a6e
Shaher-11 <shahershamroukh@gmail.com>
2021-01-20 21:37:47 +0200

    aded create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,51 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, You have successfully signed up"
+        redirect_to articles_path
+      else 
+        render 'new'
+      end 
+    end
+   
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
