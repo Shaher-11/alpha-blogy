77c3c059a10ef2ce7423f3a061b4126b864c37a9
Shaher-11 <shahershamroukh@gmail.com>
2021-01-20 22:32:06 +0200

    added the update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+     
+    def edit
+      @user = User.find(params[:id])
+    end
+  
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "Your account information was successfully updated"
+        redirect_to articles_path
+      else
+        render 'edit'
+      end
+    end
 
     def create
       @user = User.new(user_params)
