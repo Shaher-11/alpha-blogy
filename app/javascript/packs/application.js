fe73fbc43c3dd2cd8d8232a2545d6a7e8e4a0076
Shaher-11 <shahershamroukh@gmail.com>
2021-01-20 09:46:41 +0200

    added users table

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,4 @@ import "bootstrap"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_080604) do
+ActiveRecord::Schema.define(version: 2021_01_20_073834) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
