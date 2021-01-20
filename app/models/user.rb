3274f20c781d8fdaef69ee907a3dd20e53e7d06e
Shaher-11 <shahershamroukh@gmail.com>
2021-01-20 11:27:01 +0200

    more email validation aded

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
   validates :username, presence: true, 
       uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
