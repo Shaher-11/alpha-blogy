b7722f25c7013452ffdd47f6b635761177b96945
Shaher-11 <shahershamroukh@gmail.com>
2021-01-21 17:12:10 +0200

    added the homepage to users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
         end
         def create
             @article = Article.new(article_params)
-            @article.user = User.first
+            @article.user = current_user
            if @article.save
              flash[:notice] = "Article was created succefully."
             redirect_to @article
