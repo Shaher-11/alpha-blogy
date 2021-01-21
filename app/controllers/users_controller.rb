85e70525e1cccfbe3f98afb232740b5481724796
Shaher-11 <shahershamroukh@gmail.com>
2021-01-21 19:03:20 +0200

    added more erestirctions for users to be able to control thier account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
- before_action :set_user, only: [:show, :edit, :update]
-
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
      def show
       @articles = @user.articles.paginate(page: params[:page])
      end
@@ -44,6 +45,13 @@ class UsersController < ApplicationController
 
     def set_user
       @user = User.find(params[:id])
+    end
+    
+    def require_same_user
+      if current_user != @user
+        flash[:alert] = "You can only edit your own account"
+        redirect_to @user
+      end
     end
 
 end
