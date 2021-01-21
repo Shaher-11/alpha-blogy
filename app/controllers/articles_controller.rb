e5dce1d5daceda8dcf85a49ea317b88f738f2acc
Shaher-11 <shahershamroukh@gmail.com>
2021-01-21 12:40:22 +0200

    added the will_paginate gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
         end 
        
         def index
-            @articles = Article.all
+            @articles = Article.paginate(page: params[:page])
         end 
 
         def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
 
      def show
       @user = User.find(params[:id])
-      @articles = @user.articles
+      @articles = @user.articles.paginate(page: params[:page])
      end
 
      def index
-      @users = User.all
+      @users = User.paginate(page: params[:page])
      end
 
     def new
