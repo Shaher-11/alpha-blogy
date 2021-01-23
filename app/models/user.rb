a799c85c0bdd231ca29d7d7987cc242503720bc7
Shaher-11 <shahershamroukh@gmail.com>
2021-01-23 11:52:26 +0200

    changed the max for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     before_save { self.email = email.downcase }
     has_many :articles, dependent: :destroy
   validates :username, presence: true, 
-      uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+      uniqueness: { case_sensitive: false }, length: { minimum: 6, maximum: 25 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,  uniqueness: { case_sensitive: false }, 
       length: { maximum: 105 }, 
