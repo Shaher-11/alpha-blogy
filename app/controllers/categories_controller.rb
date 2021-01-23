fc58f06f77b7ba4993f830943eb0c9aa57e9497c
Shaher-11 <shahershamroukh@gmail.com>
2021-01-23 21:01:53 +0200

    added admin access for creating a new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+ 
+    before_action :require_admin, except: [:index, :show]
 
     def new
      @category = Category.new
@@ -28,7 +30,22 @@ class CategoriesController < ApplicationController
     def category_params
         params.require(:category).permit(:name)
     end
+
+    def require_admin
+     if !(logged_in? && current_user.admin?)
+        flash[:alert] = "Only admins an perform that action"
+        redirect_to categories_path
+     end
+    end
 end
+
+
+
+
+
+
+
+
 
 
 
