ca40c3f762c917744c208ef41ccfed254231cade
Shaher-11 <shahershamroukh@gmail.com>
2021-01-23 21:34:00 +0200

    added association tables

---
+++ db/migrate/20210123192100_create_article_categories.rb
@@ -0,0 +1,9 @@
+class CreateArticleCategories < ActiveRecord::Migration[6.1]
+  def change
+    create_table :article_categories do |t|
+      t.integer :article_id
+      t.integer :category_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_143840) do
+ActiveRecord::Schema.define(version: 2021_01_23_192100) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
