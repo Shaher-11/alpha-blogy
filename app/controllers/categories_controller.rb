6a41543e1afbe8245907794864c7f1151e059c89
Shaher-11 <shahershamroukh@gmail.com>
2021-01-24 10:55:34 +0200

    added edit action to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,13 +17,27 @@ class CategoriesController < ApplicationController
 
     end
 
+    def edit
+     @category = Category.find(params[:id])
+    end
+    
+    def update
+      @category = Category.find(params[:id])
+     if @category.update(category_params)
+      flash[:notice] = "Ctegory name updated successfully"
+      redirect_to @category
+     else
+      render 'edit'
+     end
+    end
+
     def index
       @categories = Category.paginate(page: params[:page], per_page: 2)
     end
 
     def show
      @category = Category.find(params[:id])
-     @articles = @category.articles.paginate(page: params[:page], per_page: 5)
+     @articles = @category.articles.paginate(page: params[:page], per_page: 2)
     end
    
     private 
