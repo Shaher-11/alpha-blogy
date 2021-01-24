caad2663f7ae0209ba8f7bba0381e32308e6d6d7
Shaher-11 <shahershamroukh@gmail.com>
2021-01-24 11:20:40 +0200

    changed the max char for the description

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ApplicationRecord
     has_many :article_categories
     has_many :categories, through: :article_categories
     validates :title, presence: true, length: { minimum: 6, maximum: 100}
-    validates :description, presence: true, length: { minimum: 100, maximum: 300}
+    validates :description, presence: true, length: { minimum: 100, maximum: 900}
 end
